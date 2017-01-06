c1955a2f40d0b2d0d427bbe057d0152d46e2c0ad
JimmieMax <cross-express@live.com>
2017-01-06 23:57:19 +0800

    demo

--- front/controller/index/routes.js
+++ front/controller/index/routes.js
@@ -2,20 +2,20 @@
 angular.module('wechat.routes', [])
     .config(function($stateProvider, $urlRouterProvider) {
         //默认状态是tab.message
-        $urlRouterProvider.otherwise("models/index/message-mobile");
+        $urlRouterProvider.otherwise("models/index/messages-mobile.html");
         $stateProvider
         //如果是tab状态被激活, 加载tabs.html模板, 注意这里的abstract: true, 表示tab只有在子状态显示的时候, 它才显示, 它本身是无法主动被激活的
             .state("tab", {
                 url: "/tab",
                 abstract: true,
-                templateUrl: "models/common/Footer.html",
+                templateUrl: "models/index/index-mobile.html"
             })
             //tab.message状态被激活,会显示tab-message.html模板, tab.message状态是在tabs.html中的ui-sref中设置的. 同时注意views中的tab-message名字, 这个也需要跟tabs.html中的ion-nav-view中的name一致哦
             .state('tab.message', {
                 url: '/message',
                 views: {
                     'tab-message': {
-                        templateUrl: 'models/index/message-mobile.html',
+                        templateUrl: 'models/index/messages-mobile.html'
                     }
                 }
             })
@@ -23,7 +23,7 @@ angular.module('wechat.routes', [])
                 url: '/friends',
                 views: {
                     'tab-friends': {
-                        templateUrl: 'models/index/friends-mobile.html',
+                        templateUrl: 'models/index/friends-mobile.html'
                     }
                 }
             })
